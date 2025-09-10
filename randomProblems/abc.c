fcde354b4b3746be69043ba8804d793fceed709a
siddhant-2029 <work.siddhantk@gmail.com>
2025-09-10 17:35:00 +0530

    new files

--- randomProblems/abc.c
+++ randomProblems/abc.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a=2,b=4,c=8;
-    if(a>b && b>c ){printf("The 2nd greatest is b");}
-    else if(b>c && c>a){printf("The 2nd greatest is c");}
-    else if(c>a && a>c) {printf("The 2nd greatest is a");}
+    float i=1.23;
+    int x=2;
+    printf("%.2f %d\n", i, x); 
+    // if(a>b && b>c ){printf("The 2nd greatest is b");}
+    // else if(b>c && c>a){printf("The 2nd greatest is c");}
+    // else if(c>a && a>c) {printf("The 2nd greatest is a");}
     return 0;
 }
